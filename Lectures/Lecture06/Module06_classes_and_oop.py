2b27141181ffac268238cb92d100b4fa37845c80
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-06-26 15:37:00 +1000

    Add labelling example from lecture

--- Lectures/Lecture06/Module06_classes_and_oop.py
+++ Lectures/Lecture06/Module06_classes_and_oop.py
@@ -1,4 +1,45 @@
 products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True]]
-on_sale_products = [product for product in products if product[2] == True]
+on_sale_products = [product for product in products if product[2]]
 print(on_sale_products)
+
+###############################################################################
+
+# 1
+# My guess:                 class
+# Answer selected:          Class header
+# Correct answer (y/n):     y
+# If no, correct answer is: ---
+
+# 2
+# My guess:                 method
+# Answer selected:          Method (function) definition
+# Correct answer (y/n):     y
+# If no, correct answer is: ---
+
+# 3
+# My guess:                 self
+# Answer selected:          Reference to this instance
+# Correct answer (y/n):     y
+# If no, correct answer is: ---
+
+# 4
+# My guess:                 variable
+# Answer selected:          This new class is a specialised version of an existing class (inheritance)
+# Correct answer (y/n):     y
+# If no, correct answer is: ---
+
+# 5
+# My guess:                 object
+# Answer selected:          Create new object (instance) of our class
+# Correct answer (y/n):     y
+# If no, correct answer is: ---
+
+# 6
+# My guess:                 attribute
+# Answer selected:          Call a method on our new object
+# Correct answer (y/n):     y
+# If no, correct answer is: ---
+
+###############################################################################
+
 
